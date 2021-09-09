464f95d2d3f344a0477cf0f3f72f69b92925b948
spooby <39270883+sp0oby@users.noreply.github.com>
2021-09-08 22:46:27 -0400

    Add files via upload

---
+++ components/Layout.tsx
@@ -0,0 +1,117 @@
+// Imports
+import Link from "next/link"; // Routing
+import { useRouter } from "next/router"; // Routing
+import { default as HTMLHead } from "next/head"; // Meta
+import styles from "@styles/components/Layout.module.scss"; // Styles
+
+// Types
+import type { ReactElement } from "react";
+
+export default function Layout({
+  children,
+}: {
+  children: ReactElement | ReactElement[];
+}) {
+  return (
+    <div>
+      {/* Meta */}
+      <Head />
+      {/* Top header */}
+      <Header />
+
+      {/* Page content */}
+      <div className={styles.content}>{children}</div>
+      {/* Bottom footer */}
+      <Footer />
+    </div>
+  );
+}
+
+/**
+ * Meta HTML Head
+ * @returns {ReactElement} HTML Head component
+ */
+function Head(): ReactElement {
+  return (
+    <HTMLHead>
+      {/* Primary Meta Tags */}
+      <title>War</title>
+      <meta name="title" content="Loot(for War)" />
+      <meta
+        name="description"
+        content="Loot(for War) is randomized war loot generated and stored on the blockchain."
+      />
+
+      {/* OG + Faceook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.lootforwar.com/" />
+      <meta property="og:title" content="Loot(for War)" />
+      <meta
+        property="og:description"
+        content="Loot(for War) is randomized war loot generated and stored on the blockchain."
+      />
+      <meta property="og:image" content="https://lootforwar.com/meta.png" />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://www.lootforwar.com/" />
+      <meta property="twitter:title" content="Loot" />
+      <meta
+        property="twitter:description"
+        content="Loot(for War) is randomized war loot generated and stored on the blockchain."
+      />
+      <meta property="twitter:image" content="" />
+
+      {/* Font */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+      <link
+        href="https://fonts.googleapis.com/css2?family=Changa:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+    </HTMLHead>
+  );
+}
+
+/**
+ * Header
+ * @returns {ReactElement} Header
+ */
+function Header() {
+  // Collect current path for active links
+  const { pathname } = useRouter();
+  // All links
+
+  return (
+    <div className={styles.header}>
+      {/* Main logo */}
+      <div className={styles.header__logo}>
+        <Link href="/">
+          <a>War</a>
+        </Link>
+      </div>
+
+      
+    </div>
+  );
+}
+
+/**
+ * Footer component
+ * @returns {ReactElement} Footer
+ */
+function Footer(): ReactElement {
+  return (
+    <div className={styles.footer}>
+      <p>
+        You can mint from the contract. Enjoy.
+
+      </p>
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,68 @@
+// Imports
+import Link from "next/link"; // Local routing
+import Layout from "@components/Layout"; // Layout wrapper
+import styles from "@styles/pages/Home.module.scss"; // Styles
+
+// Types
+import type { ReactElement } from "react";
+
+export default function Home(): ReactElement {
+  // Quicklinks to render
+  const quicklinks: Record<string, string>[] = [
+    { name: "OpenSea", url: "" },
+    {
+      name: "Twitter",
+      url: "",
+    },
+    {
+      name: "Contract",
+      url: "",
+    },
+    {
+      name: "Discord",
+      url: "https://discord.gg/vJyRDktWeN"
+    }
+  ];
+
+  return (
+    <Layout>
+      <div>
+        <div className={styles.home__cta}>
+          {/* CTA title */}
+          <h1>Loot(for War)</h1>
+
+          {/* Quicklinks */}
+          <ul>
+            {quicklinks.map(({ name, url }, i) => {
+              return (
+                <li key={i}>
+                  {url.startsWith("/") ? (
+                    // If link to local page use Link
+                    <Link href={url}>
+                      <a>{name}</a>
+                    </Link>
+                  ) : (
+                    // Else, redirect in new tab
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+                      {name}
+                    </a>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+
+          {/* CTA Description */}
+          <p>
+            Loot(for War) is randomized war loot generated and stored on the blockchain.
+            <br /> Each "Force" includes a country, support (air, land, and sea), tactic (offensive and defensive), bomb, and random surplus.<br /> Feel free to use forces in any
+            way you want. Community involvement is encouraged. 
+          </p>
+        </div>
+        
+      </div>
+    </Layout>
+  );
+}
+
+
