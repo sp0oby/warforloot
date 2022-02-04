c0ef12a767f8f4a608051298c667394c4f0f19f1
spooby <39270883+sp0oby@users.noreply.github.com>
2022-02-03 23:50:55 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import { defaultBags } from "@utils/constants";
 export default function Home(): ReactElement {
   // Quicklinks to render
   const quicklinks: Record<string, string>[] = [
-    { name: "Marketplace", url: "https://tofunft.com/discover/items?contracts=11095&network=1088" },
+    { name: "Marketplace", url: "https://tofunft.com/collection/loot-for-war/items" },
     {
       name: "Twitter",
       url: "https://twitter.com/lootforwar",
